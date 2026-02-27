f04394fd2479aecf4924646cce930d77c32a8d3b
Corey Desbrough <cdesbrough@fingerlakes.edu>
2026-02-27 00:54:29 +0000

    coin toss

--- coin_toss.py
+++ coin_toss.py
@@ -1,13 +1,24 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Corey Desbrough
+# DATE: 02/26/2026
+# BRIEF DESCRIPTION: coin toss
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
 ########## ENTER YER CODE BELOW THIS LINE ##########
+import random
 
+print("===== Coin Flipper =====")
+
+number = random.randint(1, 100)
+
+if number >= 51:
+    result = "Tails"
+else:
+    result = "Heads"
+
+print(result)
 
 
 
